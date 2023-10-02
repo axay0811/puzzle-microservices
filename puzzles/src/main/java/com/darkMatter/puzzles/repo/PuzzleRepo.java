8a2d1fbd29f3e18e82fc838168a4ded8cd84a27e
axay0811 <146705631+axay0811@users.noreply.github.com>
2023-10-02 23:53:10 +0530

    Delete puzzles directory

--- puzzles/src/main/java/com/darkMatter/puzzles/repo/PuzzleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.darkMatter.puzzles.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.darkMatter.puzzles.model.Puzzle;
-
-public interface PuzzleRepo extends JpaRepository<Puzzle, Integer>{
-
-}
